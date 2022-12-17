3eb08cad326d91c863af61b9eb7b4068525e17db
granitnebiu <granitnebiu@gmail.com>
2022-12-17 22:07:24 +0100

    add NavBar and Footer

--- ecommerce/components/NavBar.tsx
+++ ecommerce/components/NavBar.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
+import { AiOutlineShopping } from "react-icons/ai";
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Headphones</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick={() => {}}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 }
