f5a371eaee276db51e86182fab865ab3fd1059d7
granitnebiu <granitnebiu@gmail.com>
2022-12-17 21:50:16 +0100

    add Layout

--- ecommerce/components/Layout.tsx
+++ ecommerce/components/Layout.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Head from "next/head";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-export default function Layout() {
-  return <div>Layout</div>;
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Headphones E-commerce</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
